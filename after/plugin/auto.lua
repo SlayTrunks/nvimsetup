6af4887e9a378a0a96f54caa2f5412530e8ee532
slaytrunks <slaytrunks@gmail.com>
2025-01-25 21:48:36 +0545

    added tailwind suggestions

--- after/plugin/auto.lua
+++ after/plugin/auto.lua
@@ -3,22 +3,19 @@ local luasnip = require("luasnip")
 -- Define snippets
 luasnip.snippets = {
   html = {
-    -- Add your html snippets here
+    -- Add your HTML snippets here
   },
-  tailwindcss = {
-    luasnip.snippet("twc", { luasnip.text_node('class="') }),  -- A simple example snippet
-    -- Add more Tailwind CSS snippets as needed
-  },
+  -- Removed Tailwind CSS snippets
 }
 
--- Extend html and tailwindcss snippets to other file types
-luasnip.snippets.javascript = vim.tbl_deep_extend("force", {}, luasnip.snippets.html, luasnip.snippets.tailwindcss)
-luasnip.snippets.javascriptreact = vim.tbl_deep_extend("force", {}, luasnip.snippets.html, luasnip.snippets.tailwindcss)
-luasnip.snippets.typescriptreact = vim.tbl_deep_extend("force", {}, luasnip.snippets.html, luasnip.snippets.tailwindcss)
+-- Extend HTML snippets to other file types
+luasnip.snippets.javascript = vim.tbl_deep_extend("force", {}, luasnip.snippets.html)
+luasnip.snippets.javascriptreact = vim.tbl_deep_extend("force", {}, luasnip.snippets.html)
+luasnip.snippets.typescriptreact = vim.tbl_deep_extend("force", {}, luasnip.snippets.html)
 
 -- Load snippets from VSCode
 require("luasnip/loaders/from_vscode").lazy_load()
 
--- Extend file types with html and tailwindcss snippets
-require('luasnip').filetype_extend("javascriptreact", { "html", "tailwindcss" })
-require('luasnip').filetype_extend("typescriptreact", { "html", "tailwindcss" })
+-- Extend file types with HTML snippets only
+require('luasnip').filetype_extend("javascriptreact", { "html" })
+require('luasnip').filetype_extend("typescriptreact", { "html" })
